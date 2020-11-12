03162f4d33a6890a5b0c75e94ee851b1d8345ffe
alihaider123go <alihaider123go@gmail.com>
2020-11-12 11:49:33 +0500

    my first commit

--- components/Header.vue
+++ components/Header.vue
@@ -2,13 +2,13 @@
   <header class="topnav h-54px p-2 border-b-2 border-gray-200">
     <div class="container mx-auto flex justify-between items-center">
       <div class="right flex justify-start items-center">
-        <Logo class="w-8 h-8 mr-3" />
-        <h1 class="text-lg font-bold text-gray-700">{{ title }}</h1>
+        <!-- <Logo class="w-8 h-8 mr-3" /> -->
+        <h1 class="text-lg font-bold text-gray-700">{{ $t(title) }}</h1>
       </div>
       <div class="left self-end">
         <ul class="flex lang-switch justify-around" v-if="!isContentPage">
             <li v-if="$i18n.locale !== 'en'">
-                <nuxt-link class="text-md ...":to="switchLocalePath('en')">EN</nuxt-link>
+                <nuxt-link class="text-md ..." :to="switchLocalePath('en')">EN</nuxt-link>
             </li>
             <li v-if="$i18n.locale !== 'fr'">
                 <nuxt-link class="text-md ..." :to="switchLocalePath('fr')">FR</nuxt-link>
@@ -21,7 +21,12 @@
 
 <script>
 export default {
-  name: 'Header',
+  name: 'Header-Component',
+  data() {
+      return {
+          title:'Header',
+      }
+  },
   computed: {
     isContentPage() {
       return this.$route.name.includes('slug');
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-    <Header />
-    <Nuxt />
-    <ColorModePicker />
+    <Header/>
+    <header></header>
+    <nuxt/>
+    <color-mode-picker></color-mode-picker>
   </div>
 </template>
 
 <script>
+import Header from '~/components/Header.vue';
+import ColorModePicker from '~/components/molecules/ColorModePicker';
+
 export default {
-  name:'default'
+  name:'default',
+  components:{
+    Header,
+    ColorModePicker
+  }
 }
 </script>
