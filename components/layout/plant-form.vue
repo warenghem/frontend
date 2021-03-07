652ffefd77a1a1b98e02ca55e964baa8eb20a7bd
kbrosseau <kevin.brosseau@warenghem.com>
2021-03-07 13:20:01 +0100

    add

--- components/layout/plant-form.vue
+++ components/layout/plant-form.vue
@@ -1,139 +1,190 @@
 <template>
-    <div>
-        <div class="mx-auto text-center">
-            <v-dialog
+  <div>
+    <div class="mx-auto text-center">
+      <v-dialog
                     v-model="$store.state.newsletterModal"
-                    persistent
-                    max-width="600px"
+        content-class="bg-white bottom-dialog rounded-b-0 rounded-t-lg"
+        persistent
+        max-width="700px"
+      >
+        <v-card>
+          <div style="height: 50px;" class="d-flex justify-space-between align-center border-bottom-2">
+            <div class="sub-title pl-3">{{$t('btnWaitforit')}}</div>
+            <v-btn
+              text
+              color="black"
+              style="font-size: 26px"
+              class="px-0 h-100 border-left-2 rounded-0"
+              @click="$store.state.productModal = false"
             >
-                <v-card>
-                    <div style="height: 50px;" class="d-flex justify-space-between align-center border-bottom-2">
-                        <div class="sub-title pl-3">{{$t('video.rightSection.title')}}</div>
-                        <v-btn
-                        text
-                        color="black"
-                        style="font-size: 26px"
-                        class="px-0 h-100 border-left-2 rounded-0"
-                        @click="$store.state.newsletterModal = false"
+              <v-icon>{{ svgPath }}</v-icon>
+            </v-btn>
+          </div>
+          <v-card-text class="pvw">
+            <div class="d-flex flex-column align-center justify-center h-100">
+              <p class="pb-3 px-4 text-justify bottomText" v-html="$t('video.rightSection.subtitle')">
+              </p>
+              <div class="w-100 px-sm-5">
+                <mailchimp-subscribe
+                  url="https://warenghem.us20.list-manage.com/subscribe/post-json"
+                  user-id="2f7dbf20677f88c14c1389365"
+                  list-id="ee2afb40a2"
+                  @error="snackbarError=true"
+                  @success="snackbarSuccess=true"
+                >
+                  <template v-slot="{ subscribe, setEmail, setName, loading }">
+                    <validation-observer
+                      ref="observer"
+                    >
+                      <v-form @submit.prevent="subscribe" class="d-flex flex-column w-100" >
+                        <validation-provider
+                          v-slot="{ errors }"
+                          name="Name"
+                          rules="required|max:20"
                         >
-                        <v-icon>{{ svgPath }}</v-icon>
-                        </v-btn>
-                    </div>
-                    <v-card-text
-                    class="pvw">
-                        <div class="d-flex flex-column align-center justify-center h-100 pb-5">
-                            <p class="pb-3 text-justify bottomText" v-html="$t('video.rightSection.subtitle')">
-                            </p>
-                            <div class="w-100">
-                                <mailchimp-subscribe
-                                        url="https://warenghem.us20.list-manage.com/subscribe/post-json"
-                                        user-id="2f7dbf20677f88c14c1389365"
-                                        list-id="ee2afb40a2"
-                                        @error="snackbarError=true"
-                                        @success="snackbarSuccess=true"
-                                >
-                                    <template v-slot="{ subscribe, setEmail, setName,loading }">
-                                        <form @submit.prevent="subscribe" class="d-flex flex-column  w-100">
-                                            <input type="text"
-                                                   class="form-theme"
-                                                   :placeholder="$t('video.rightSection.label1')"
-                                                   @input="setName($event.target.value)"
-                                            >
-                                            <input type="email"
-                                                   class="form-theme"
-                                                   :placeholder="$t('video.rightSection.label2')"
-                                                   @input="setEmail($event.target.value)"
-                                            >
-                                            <div class="mx-auto">
-                                                    <v-btn
-                                                        elevation="0"
-                                                        large
-                                                        :loading="loading"
-                                                        class="btn-theme"
-                                                        type="submit"
-                                                        style="max-width: 250px;border-radius: 28px;word-break: break-word;outline: 0;display: inline-block;white-space: normal;"
-                                                        >
-                                                            {{$t('btnDiscover')}}
-                                                    </v-btn>
-                                            </div>
-                                        </form>
-                                    </template>
-                                </mailchimp-subscribe>
-                            </div>
+                          <v-text-field
+                            v-model="name"
+                            required
+                            :counter="20"
+                            :error-messages="errors"
+                            clearable
+                            color="lightbugattiblue"
+                            :prepend-icon="svgPath3"
+                            :label="$t('video.rightSection.label1')"
+                            type="text" value="" @input="setName(name)"
+                          ></v-text-field>
+                        </validation-provider>
+                        <validation-provider
+                          v-slot="{ errors }"
+                          name="email"
+                          rules="required|email"
+                        >
+                          <v-text-field
+                            clearable
+                            required
+                            color="lightbugattiblue"
+                            :prepend-icon="svgPath2"
+                            :label="$t('video.rightSection.label2')"
+                            v-model="email"
+                            :error-messages="errors"
+                            type="email"
+                            value=""
+                            @input="setEmail(email)"
+                          ></v-text-field>
+                        </validation-provider>
+
+                        <div class="mx-auto">
+                          <v-btn
+                            elevation="0"
+                            large
+                            :loading="loading"
+                            class="btn-theme"
+                            type="button"
+                            @click.prevent="submitForm"
+                            style="max-width: 250px;border-radius: 28px;word-break: break-word;outline: 0;display: inline-block;white-space: normal;"
+                          >
+                            {{$t('btnDiscover')}}
+                          </v-btn>
+                          <button class="hidden" type="submit" ref="mailForm"></button>
                         </div>
-                        <v-card-subtitle class="text-justify">
-                            {{$t('newsletterdisclaimer')}}
-                        </v-card-subtitle>
-                    </v-card-text>
-                </v-card>
-            </v-dialog>
-        </div>
-        <v-snackbar
-            v-model="snackbarSuccess"
-            timeout="7500"
-            rounded="lg"
-            >
-            <div>
-                <div class="d-flex justify-space-between align-center">
-                    <div>{{$t('video.rightSection.alerttitle')}}</div>
-                    <div class="d-flex align-center"><small class="pr-2">{{$t('video.rightSection.alerttime')}} </small>
-                        <v-btn icon class="close" @click="snackbarSuccess=false">
-                            <v-icon>{{ svgPath }}</v-icon>
-                        </v-btn>
-                    </div>
-                </div>
-                <v-layout align-center mt-5 mb-2 pr-4>
-                    <v-icon color="blue darken-2" class="pr-3" dark large>mdi-checkbox-marked-circle</v-icon>
-                    <v-layout column>
-                        <div>{{$t('video.rightSection.success')}}</div>
-                    </v-layout> 
-                </v-layout>
+                      </v-form>
+                    </validation-observer>
+                  </template>
+                </mailchimp-subscribe>
+              </div>
             </div>
-        </v-snackbar>
-        <v-snackbar
-            v-model="snackbarError"
-            timeout="-1"
-            rounded="lg"
-            >
-            <div>
-                <div class="d-flex justify-space-between align-center">
-                    <div>{{$t('video.rightSection.alerttitle')}}</div>
-                    <div class="d-flex align-center">
-                        <small class="pr-2">{{$t('video.rightSection.alerttime')}} </small>
-                        <v-btn icon class="close" @click="snackbarError=false">
-                            <v-icon>{{ svgPath }}</v-icon>
-                        </v-btn>
-                    </div>
-                </div>
-                <v-layout align-center mt-5 mb-2 pr-4>
-                    <v-icon color="orange darken-2" class="pr-3" dark large>mdi-alert-circle-outline</v-icon>
-                    <v-layout column>
-                        <div>{{$t('video.rightSection.error')}} <a style="color:white" href="mailto:hello@warenghem.com">hello@warenghem.com</a></div>
-                    </v-layout>
-                </v-layout>
-            </div>
-        </v-snackbar>
+            <v-card-subtitle class="text-justify">
+              {{$t('newsletterdisclaimer')}}
+            </v-card-subtitle>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </div>
+    <v-snackbar
+      v-model="snackbarSuccess"
+      timeout="7500"
+      rounded="lg"
+    >
+      <div>
+        <div class="d-flex justify-space-between align-center">
+          <div>{{$t('video.rightSection.alerttitle')}}</div>
+          <div class="d-flex align-center"><small class="pr-2">{{$t('video.rightSection.alerttime')}} </small>
+            <v-btn icon class="close" @click="snackbarSuccess=false">
+              <v-icon>{{ svgPath }}</v-icon>
+            </v-btn>
+          </div>
+        </div>
+        <v-layout align-center mt-5 mb-2 pr-4>
+          <v-icon color="blue darken-2" class="pr-3" dark large>mdi-checkbox-marked-circle</v-icon>
+          <v-layout column>
+            <div>{{$t('video.rightSection.success')}}</div>
+          </v-layout>
+        </v-layout>
+      </div>
+    </v-snackbar>
+    <v-snackbar
+      v-model="snackbarError"
+      timeout="-1"
+      rounded="lg"
+    >
+      <div>
+        <div class="d-flex justify-space-between align-center">
+          <div>{{$t('video.rightSection.alerttitle')}}</div>
+          <div class="d-flex align-center">
+            <small class="pr-2">{{$t('video.rightSection.alerttime')}} </small>
+            <v-btn icon class="close" @click="snackbarError=false">
+              <v-icon>{{ svgPath }}</v-icon>
+            </v-btn>
+          </div>
+        </div>
+        <v-layout align-center mt-5 mb-2 pr-4>
+          <v-icon color="orange darken-2" class="pr-3" dark large>mdi-alert-circle-outline</v-icon>
+          <v-layout column>
+            <div>{{$t('video.rightSection.error')}} <a style="color:white" href="mailto:hello@warenghem.com">hello@warenghem.com</a>
+            </div>
+          </v-layout>
+        </v-layout>
+      </div>
+    </v-snackbar>
+  </div>
 
 </template>
 
 <script>
-    import MailchimpSubscribe from './mailchp-subscribe'
-    import { mdiClose } from '@mdi/js'
+    import MailchimpSubscribe from './mailchp-subscribe-products'
+    import {mdiClose, mdiEmail, mdiAccount} from '@mdi/js'
+    import {ValidationObserver, ValidationProvider} from "vee-validate";
     export default {
-        name: "newsletter-form",
+        name: "products-form",
         components: {
             MailchimpSubscribe,
+            ValidationProvider,
+            ValidationObserver,
         },
         data() {
             return {
+                product: [],
                 snackbarSuccess: false,
                 snackbarError: false,
                 email: '',
                 loading: false,
                 svgPath: mdiClose,
+                isFormValid: false,
+                items: ['Francais', 'English'],
+                svgPath2: mdiEmail,
+                svgPath3: mdiAccount,
+                name: '',
             }
         },
+        methods: {
+            async submitForm() {
+                const isValid = await this.$refs.observer.validate()
+                if (isValid) {
+                    console.log(this.$refs.mailForm)
+                    this.$refs.mailForm.click()
+                }
+            }
+        }
     }
 </script>
 <i18n>
@@ -146,7 +197,6 @@
 				"error": "Your email appears to be invalid or already registered. Contact us:",
 				"label1": "First Name",
 				"label2": "e-mail",
-                "title": "Plant a tree",
 				"subtitle": "Our objects are being created. In the meantime, let's reforest the planet. Enter your email address to plant a tree and stay informed about the progress of the project.",
 				"success": "Thank you! To avoid SPAM, we have sent you a confirmation email before starting the planting ... See you soon!",
 				"tag": "Creation <br> of 3D objects",
@@ -162,8 +212,7 @@
 				"error": "Votre email semble invalide ou déjà enregistré. Contactez nous:",
 				"label1": "PRENOM ",
 				"label2": "EMAIL",
-                "title": "Planter un arbre",
-				"subtitle": "Nous objets sont en cours de création. En attendant, reforestons la planète. Rentrez votre addresse mail pour planter un arbre et rester au courant de l’avancée du projet.",
+				"subtitle": "30 examplaires par produit sont en cours de production. Reservez les avant qu'il ne soit trop tard, aucun paiement n'est requis !",
 				"success": "Merci! Pour éviter le SPAM, nous vous avons envoyé un email de confirmation avant de commencer la plantation... A tout de suite!",
 				"tag": "Création <br> d’objets en 3D",
 				"title": "Avancement du projet"
@@ -173,5 +222,31 @@
 }
 </i18n>
 <style scoped>
+  ul {
+    list-style-type: none;
+  }
 
+  li {
+    display: inline-block;
+  }
+
+  input[type="checkbox"][id^="mce-"] {
+    display: none !important;
+  }
+
+  label {
+    border: 1px solid #fff;
+    display: block;
+    position: relative;
+    margin: 10px;
+    cursor: pointer;
+    transition: box-shadow 0.2s linear, margin 0.2s linear;
+  }
+
+  :checked + label {
+    box-shadow: 0px 0px 0px 2px #3D8EBE;
+    transition: box-shadow 0.2s linear, margin 0.2s linear;
+    margin: 0.5em;
+    outline: solid 10px transparent;
+  }
 </style>
