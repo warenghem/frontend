55f781fc02a73b96f90cbf9b402a67392c36ee5f
kbrosseau <kevin.brosseau@warenghem.com>
2021-03-23 10:57:18 +0100

    add

--- components/Homecards/mission-section.vue
+++ components/Homecards/mission-section.vue
@@ -54,7 +54,7 @@
                         <div class="pvw text-center">
                             <h2 class="home-title teradeli-medium px-0 text-white">{{$t('section3.title')}}</h2>
                             <div class="pt-2 home-subtitle text-white">{{$t('section3.subtitle')}}</div>
-                            <Discoverbutton class="text-white pt-3 position-relative" style="font-size:16px"/>
+                            <Discoverbutton class="text-white theme--dark pt-3 position-relative" style="font-size:16px"/>
                         </div>
                         <div>
                             <img
