1c8f85fcd8c56571d1fc8af4217be4e147b58ca6
warenghem <kevin.brosseau@warenghem.com>
2021-04-22 19:45:34 +0200

    add

--- plugins/lazysizes.js
+++ plugins/lazysizes.js
@@ -1,5 +1,5 @@
 import lazySizes from 'lazysizes'
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+/*import 'lazysizes/plugins/parent-fit/ls.parent-fit';*/
 /*import 'lazysizes/plugins/blur-up/ls.blur-up';*/
 
 export default lazySizes
