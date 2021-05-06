07f4bff078884baca86b41fde23d468cc8a74ab5
kbrosseau <kevin.brosseau@warenghem.com>
2021-05-06 23:04:04 +0200

    add

--- components/organisms/map.vue
+++ components/organisms/map.vue
@@ -283,6 +283,14 @@
     import { LMap, LTileLayer, LMarker, LIcon } from 'vue2-leaflet';
     import 'leaflet/dist/leaflet.css';
     import { mdiClose } from '@mdi/js';
+    import L from 'leaflet';
+    delete L.Icon.Default.prototype._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+    });
     export default {
         name: 'leaflet-map',
         components: {
