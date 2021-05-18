ca03fef64ccdea7892cdc365a3f72a96b418be3f
warenghem <kevin.brosseau@warenghem.com>
2021-05-18 16:11:53 +0200

    qrcode

--- components/SustainableTech/qrcodereader.vue
+++ components/SustainableTech/qrcodereader.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    <qrcode-capture v-if="noStreamApiSupport" :track="lightbuggatiblue" :torch="torchActive" @init="onInit" @decode="onDecode">
-        <div class="loading-indicator" v-if="loading">
-          Loading...
-        </div>
-        <button @click="torchActive = !torchActive" :disabled="torchNotSupported">
-          <img :src="icon" alt="toggle torch">
-        </button>
-    </qrcode-capture>
+    <p class="decode-result">Last result: <b>{{ result }}</b></p>
+    <qrcode-stream :camera="camera" v-if="noStreamApiSupport" :track="lightbuggatiblue" :torch="torchActive" @decode="onDecode" @init="onInit">
+      <div class="loading-indicator" v-if="loading">
+        <v-skeleton-loader
+          class="mx-auto"
+          max-width="300"
+          type="image"
+        ></v-skeleton-loader>
+      </div>
+      <div v-if="validationSuccess" class="validation-success">
+        This is a URL
+      </div>
+
+      <div v-if="validationFailure" class="validation-failure">
+        This is NOT a URL!
+      </div>
+
+      <div v-if="validationPending" class="validation-pending">
+        Long validation in progress...
+      </div>
+      <button @click="torchActive = !torchActive" :disabled="torchNotSupported">
+        <img :src="icon" alt="toggle torch">
+      </button>
+    </qrcode-stream>
     <qrcode-drop-zone @decode="onDecode" @init="logErrors">
       <qrcode-stream @decode="onDecode" @init="onInit" />
     </qrcode-drop-zone>
@@ -24,13 +40,27 @@
     data () {
       return {
         loading: false,
-        result: '',
+        isValid: undefined,
+        camera: 'auto',
+        result: null,
         error: '',
         torchActive: false,
         torchNotSupported: false
       }
     },
     computed: {
+      validationPending () {
+        return this.isValid === undefined
+          && this.camera === 'off'
+      },
+
+      validationSuccess () {
+        return this.isValid === true
+      },
+
+      validationFailure () {
+        return this.isValid === false
+      },
       icon() {
         if (this.torchActive)
           return '/flash-off.svg'
@@ -39,10 +69,6 @@
       }
     },
     methods: {
-      onDecode (url) {
-        window.location.href = url
-      },
-
       async onInit (promise) {
         this.loading = true
         try {
@@ -72,7 +98,38 @@
           }
         } finally {
         this.loading = false
+        this.resetValidationState
       }
+      },
+      resetValidationState () {
+        this.isValid = undefined
+      },
+      async onDecode (content) {
+        this.result = content
+        this.turnCameraOff()
+
+        // pretend it's taking really long
+        await this.timeout(3000)
+        this.isValid = content.startsWith('https://www.warenghem.com')
+
+        // some more delay, so users have time to read the message
+        await this.timeout(2000)
+        window.location.href = url
+
+        this.turnCameraOn()
+      },
+      turnCameraOn () {
+        this.camera = 'auto'
+      },
+
+      turnCameraOff () {
+        this.camera = 'off'
+      },
+
+      timeout (ms) {
+        return new Promise(resolve => {
+          window.setTimeout(resolve, ms)
+        })
       }
     },
 
@@ -109,5 +166,28 @@ button {
 .error {
   color: red;
   font-weight: bold;
+}
+.validation-success,
+.validation-failure,
+.validation-pending {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba(255, 255, 255, .8);
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.4rem;
+  padding: 10px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+}
+.validation-success {
+  color: green;
+}
+.validation-failure {
+  color: red;
 }
 </style>
