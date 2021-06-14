3d6d2167065db42b01333c8a1c8198cb0cc7f578
kbrosseau <kevin.brosseau@warenghem.com>
2021-06-14 19:41:49 +0200

    aadd

---
+++ pages/origin/_productid/_productindex/trip.vue
@@ -0,0 +1,27 @@
+<template dark>
+  <v-container style="max-width:600px" class="pl-0 timeline-container mx-auto">
+      <OriginTimeline :treesItem="$attrs.treesItem" :productsItem="$attrs.productsItem" :certificatesItem="$attrs.certificatesItem" :providersItem="$attrs.providersItem" :originTimelines="orderedTransactions"/>
+  </v-container>
+</template>
+
+<script>
+
+  export default {
+    layout: 'app',
+    name: 'trip',
+    head () {
+      return {
+        meta: [
+          { hid: 'robots', name: 'robots', content: 'noindex' }
+        ]
+      }
+    },
+    props: {
+        orderedTransactions: {
+            type: Array,
+            default: () => {
+            }
+        }
+    },
+  }
+</script>
